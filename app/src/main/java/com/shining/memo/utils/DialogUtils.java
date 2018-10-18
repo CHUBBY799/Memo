716ef816331130e3ba7fa1b7056340c740aab34a
zepeng.qiu <zepeng.qiu@tcl.com>
2018-10-18 12:42:44 +0800

    main page fix

---
+++ app/src/main/java/com/shining/memo/utils/DialogUtils.java
@@ -0,0 +1,26 @@
+package com.shining.memo.utils;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.shining.memo.R;
+
+
+public class DialogUtils {
+    public static void showDialog(Context context, String title, String message
+            , DialogInterface.OnClickListener positive
+            , DialogInterface.OnClickListener negative){
+        AlertDialog dialog = new AlertDialog.Builder(context)
+        .setTitle(title)
+        .setMessage(message)
+        .setCancelable(true)
+        .setPositiveButton("OK", positive)
+                .setNegativeButton("Cancel",negative).create();
+        dialog.show();
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(context.getColor(R.color.textcolor_orange));
+        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(context.getColor(R.color.textcolor_orange));
+
+    }
+}
